eba6bdb83f597faba0269cb5ce7f19e09c1102d5
ivanBeginning <80164349+ivanBeginning@users.noreply.github.com>
2021-03-08 18:44:29 +0300

    Add files via upload

---
+++ 06_missions_3.js
@@ -0,0 +1,14 @@
+
+function march(girl)
+{
+    for (let jul = 0; jul <= girl; jul++)
+    {
+        if (jul === 5) {console.log ("five")}
+        else if (jul === 13) {console.log ("thirteen")}
+        else if (jul === 22) {console.log ("twenty two")}
+        else if (jul === 35) {console.log ("thirty five")}
+        else if (jul === 98) {console.log ("ninety eight")}
+        console.log (jul)
+    }    
+}
+console.log(march(100))
